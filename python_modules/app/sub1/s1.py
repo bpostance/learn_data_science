0075f275d13280d84a090308ccdab5cee2bcde2e
pen-b <b.postance@lboro.ac.uk>
2019-04-13 07:38:21 +0100

    python modules and sub-modules

---
+++ python_modules/app/sub1/s1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 07:28:21 2019
+
+@author: Ben
+"""
+
+from s1_params import param1
+
+class Sub1:
+    
+    def sub1():
+        print(param1)
+    
---
+++ python_modules/app/sub2/s2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 07:28:21 2019
+
+@author: Ben
+"""
+
+from s2_params import param1
+
+class Sub2:
+    
+    def sub2():
+        print(param1)
+    
