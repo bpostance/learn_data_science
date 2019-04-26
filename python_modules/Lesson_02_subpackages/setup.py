23ccfea1decc209612725a6d7d46bc17cb2df8ad
pen-b <b.postance@lboro.ac.uk>
2019-04-26 16:19:26 +0100

    added spacy demo

---
+++ python_modules/Lesson_02_subpackages/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Lesson_01_packages',
+    version='0.1dev',
+    url='My_App_Website.com',
+    author='Old Smaug',
+    author_email='ask_me_later@email.com',
+    packages=['my_app',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+)
