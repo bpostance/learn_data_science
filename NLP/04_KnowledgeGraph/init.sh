eedac92cb5f4d462490084f73bb15fa02c722a6e
pen-b <b.postance@lboro.ac.uk>
2022-01-23 14:55:03 +0000

    add pip timeout fo rlarge download

--- NLP/04_KnowledgeGraph/init.sh
+++ NLP/04_KnowledgeGraph/init.sh
@@ -4,8 +4,8 @@
 conda env create -f ./environment.yaml
 conda activate py37
 
-# install spacy large model
-pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.3.1/en_core_web_lg-2.3.1-fix.tar.gz
+# install spacy large model 700mb
+pip --default-timeout=1000 install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.3.1/en_core_web_lg-2.3.1-fix.tar.gz
 
 # install neuralcoref from source
 git clone https://github.com/huggingface/neuralcoref.git
