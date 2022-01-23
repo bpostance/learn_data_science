9b95c038edd068a265af81435612b543bc9b8495
pen-b <b.postance@lboro.ac.uk>
2022-01-23 15:06:17 +0000

    spacy version init

--- NLP/04_KnowledgeGraph/init.sh
+++ NLP/04_KnowledgeGraph/init.sh
@@ -5,7 +5,7 @@ conda env create -f ./environment.yaml
 conda activate py37
 
 # install spacy large model 700mb
-pip --default-timeout=1000 install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.3.1/en_core_web_lg-2.3.1-fix.tar.gz
+pip --default-timeout=1000 install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.1.0/en_core_web_lg-2.1.0.tar.gz
 
 # install neuralcoref from source
 git clone https://github.com/huggingface/neuralcoref.git
